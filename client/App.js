1a3df16096b0db654c966afd029345c83098bc61
Marcin Ratajczak <maratajczak@gmail.com>
2019-02-05 21:32:47 +0100

    Add other methods

--- client/App.js
+++ client/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import io from 'socket.io-client';
 
+import styles from './App.css';
+
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
+import UsersList from './UsersList';
+import UserForm from './UserForm';
+
 const socket = io('/');
 
 class App extends Component {
@@ -14,6 +21,31 @@ class App extends Component {
 			name: '',
 			color: ''
 		};
+    }
+
+    componentDidMount() {
+        socket.on('message', message => this.messageReceive(message));
+        socket.on('update', ({users}) => this.chatUpdate(users));
+    }
+
+    messageReceive(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({messages});
+    }  
+
+    chatUpdate(users) {
+        this.setState({users});
+    }
+
+    handleMessageSubmit(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({messages});
+        socket.emit('message', message);
+    }
+
+    handleUserSubmit(name) {
+        this.setState({name});
+        socket.emit('join', name);
     }
 
     render() {
@@ -50,5 +82,10 @@ class App extends Component {
             </div>
         );
     }
-}
+    renderUserForm() {
+        return (<UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
+    }
+};
+
+export default hot(module)(App);
 
